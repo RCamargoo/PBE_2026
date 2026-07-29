0161721198dfd7adfbfdab8cf89602df369982f3
RAFAEL DE CAMARGO BRICOLERI <rafael.bricoleri@portalsesisp.org.br>
2026-07-29 13:18:42 -0300

    20/07  13:18

---
+++ 01Aula/02atividade.php
@@ -0,0 +1,19 @@
+<?php
+    //declaração varival 
+    $nome = "Estudante";
+    $idade = 20;
+
+    //Exibindo informação
+
+    echo "Olá, $nome!";
+    echo "Você tem $idade anos. ";
+
+    //condisional
+    if($idade >= 18){
+        echo "você é maior de idede";
+    }
+    else{
+        echo "Você é de idade ";
+    }
+
+?>
